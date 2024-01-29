9ef4481201d17ae74c41926682a59d06dd634f77
dude363635 <143981761+dude363635@users.noreply.github.com>
2024-01-30 07:18:16 +1000

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,7 +1,7 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = (req, res) => {
-  let target = "https://www.chat.openai.com/";//your website url
+  let target = "https://www.bloxd.io/";//your website url
   //   if (
   //     req.url.startsWith("/api") ||
   //     req.url.startsWith("/auth") ||
